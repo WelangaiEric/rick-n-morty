399a184502e25675f43303e22fb8d00f395e9537
welangaieric <welangaieric@gmail.com>
2023-05-23 23:29:20 +0300

    rick and morty scary

---
+++ app.js
@@ -0,0 +1,29 @@
+document.addEventListener('mousemove',(e)=>{
+    // console.log(e)
+    const mouseX = e.clientX
+    const mouseY= e.clientY
+
+    const anchor = document.querySelector('#anchor')
+    const rect = anchor.getBoundingClientRect();
+    const anchorX = rect.left+rect.width/2;
+    const anchorY = rect.top+rect.height/2;
+    const angleDeg = angle(mouseX,mouseY,anchorX,anchorY)
+    console.log(angleDeg)
+
+    const eyes = document.querySelectorAll('.eye')
+    eyes.forEach(eye=>{
+        eye.style.transform = `rotate(${90+angleDeg}deg)`;
+    })
+
+
+
+})
+function angle(cx,cy,ex,ey){
+    const dy = ey - cy
+    const dx = ex  - cx
+
+    const rad = Math.atan2(dy,dx);
+    const deg = rad*180/Math.PI;
+    return deg;
+
+}
